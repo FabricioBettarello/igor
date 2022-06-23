3300c846230bc66e2dd8e44703232c4d968b5923
Fabricio <100129385+FabricioBettarello@users.noreply.github.com>
2022-06-23 13:13:02 -0300

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,27 @@
+class Geral{
+    constructor(bebida, lanche, batata){
+        this.bebida = bebida;
+        this.lanche = lanche;
+        this.batata = batata;
+    }
+}
+
+class Lanche{
+    constructor(McLanche, Big, McHen) { 
+        this.McLanche = McLanche;
+        this.Big = Big;
+        this.lanchonetenome = lanchonetenome;
+    }
+}
+
+venda(pedido, valor) {
+        if (cpf === conta.cpf) {
+            //Essa função envia os valores ao banco do funcionário
+            //já chequei os parâmetros de envio e estão certos
+            bankApi.addValueBank(conta, valor);
+    }
+}
+
+
+
+
