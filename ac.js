2ff2daaf8717ccf2028d0fb594952ad5de8d01b2
Fabricio <100129385+FabricioBettarello@users.noreply.github.com>
2022-06-23 14:50:09 -0300

    Add files via upload

--- ac.js
+++ ac.js
@@ -1,31 +1,36 @@
-class McLanche {
-    constructor(carne, queijo, batatap, fruta) {
-        this.carne = carne;
-        this.queijo = queijo;
-        this.batatap = batatap;
-        this.fruta = fruta;
-    }
-}
+class Lanchonete {
+    constructor(McLanche, Big, McHen) {
 
-class Big {
-    constructor(alface, queijo, molho, cebola, picles, pao, batatam, bebida) {
-        this.alface = alface;
-        this.queijo = queijo;
-        this.molho = molho;
-        this.cebola = cebola;
-        this.picles = picles;
-        this.pao = pao;
-        this.batatam = batatam;
-        this.bebida = bebida;
-    }
-}
+        class McLanche {
+            constructor(carne, queijo, batatap, fruta) {
+                this.carne = carne;
+                this.queijo = queijo;
+                this.batatap = batatap;
+                this.fruta = fruta;
+            }
+        }
 
-class McHen {
-    constructor(alface, queijo, picles, batatap, batatam) {
-        this.alface = alface;
-        this.queijo = queijo;
-        this.picles = picles;
-        this.batatap = batatap;
-        this.batatam = batatam;
+        class Big {
+            constructor(alface, queijo, molho, cebola, picles, pao, batatam, bebida) {
+                this.alface = alface;
+                this.queijo = queijo;
+                this.molho = molho;
+                this.cebola = cebola;
+                this.picles = picles;
+                this.pao = pao;
+                this.batatam = batatam;
+                this.bebida = bebida;
+            }
+        }
+
+        class McHen {
+            constructor(alface, queijo, picles, batatap, batatam) {
+                this.alface = alface;
+                this.queijo = queijo;
+                this.picles = picles;
+                this.batatap = batatap;
+                this.batatam = batatam;
+            }
+        }
     }
 }
